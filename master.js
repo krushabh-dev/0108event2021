b6e218df8e077d637cdcd3c568b2e94847d29bfa
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2021-07-28 14:37:39 +0530

    Update master.js

---
+++ master.js
@@ -0,0 +1,23 @@
+var zf_DateRegex = new RegExp(
+  "^(([0][1-9])|([1-2][0-9])|([3][0-1]))[-](Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)[-](?:(?:19|20)[0-9]{2})$"
+);
+var zf_MandArray = [
+  "Name_First",
+  "Name_Last",
+  "SingleLine",
+  "Email",
+  "PhoneNumber_countrycode",
+  "PhoneNumber_countrycodeVal",
+  "DecisionBox",
+];
+var zf_FieldArray = [
+  "Name_First",
+  "Name_Last",
+  "SingleLine",
+  "Email",
+  "PhoneNumber_countrycode",
+  "PhoneNumber_countrycodeVal",
+  "DecisionBox",
+];
+var isSalesIQIntegrationEnabled = false;
+var salesIQFieldsArray = [];
