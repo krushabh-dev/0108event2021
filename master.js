6205e98fb7779743367b9c7db19de1784efcb870
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2021-07-28 15:28:40 +0530

    Update master.js

--- master.js
+++ master.js
@@ -21,3 +21,31 @@ var zf_FieldArray = [
 ];
 var isSalesIQIntegrationEnabled = false;
 var salesIQFieldsArray = [];
+
+var countDownDate = new Date("Aug 1, 2021 11:00:00").getTime();
+
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("time-days").innerHTML = days;
+  document.getElementById("time-hours").innerHTML = hours;
+  document.getElementById("time-minutes").innerHTML = minutes;
+  document.getElementById("time-seconds").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
